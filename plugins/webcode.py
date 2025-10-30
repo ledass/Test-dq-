c0d9a65e112446045828affe99438ce3d653f5f7
AfsalHashim <138692053+CyberTG@users.noreply.github.com>
2025-10-30 20:20:08 +0530

    Update webcode.py

--- plugins/webcode.py
+++ plugins/webcode.py
@@ -1,13 +1,7 @@
-from aiohttp import web as webserver
+from aiohttp import web
 
-routes = webserver.RouteTableDef()
+routes = web.RouteTableDef()
 
-async def bot_run():
-    _app = webserver.Application(client_max_size=30000000)
-    _app.add_routes(routes)
-    return _app
-  
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return webserver.json_response("Goutham Josh KuttuBot Web Supported . . . !  This is a preview of WeB . . .! ! !")
- 
+    return web.json_response("CyberTG")
